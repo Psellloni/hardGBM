ea51d52d450dadfc1c94f0b18c843c78eb26c772
pselloni <kasenkolev318@gmail.com>
2024-02-03 13:02:46 +0300

    feat: added mae

--- model.cpp
+++ model.cpp
@@ -1,25 +1,53 @@
 #include<iostream>
 #include<vector>
 
-double mse(std::vector<int> a, std::vector<int> b) {
+class Metrics {
+public:
+	double mse(std::vector<int> a, std::vector<int> b) {
 
-	double result = 0;
+		if (a.size() != b.size()) {
+			return -1;
+		}
 
-	for (int i = 0; i < a.size(); i ++) {
-		result += ((a[i] - b[i]) * (a[i] - b[i]));
-	}
+		double result = 0;
+
+		for (int i = 0; i < a.size(); i ++) {
+			result += ((a[i] - b[i]) * (a[i] - b[i]));
+		}
 	
-	result /= a.size();
+		result /= a.size();
 
-	return result;
-}
+		return result;
+
+		}
+
+	double mae(std::vector<int> a, std::vector<int> b) {
+
+		if (a.size() != b.size()) {
+			return -1;
+		}
+
+		double result = 0;
+
+		for (int i = 0; i < a.size(); i ++) {
+			result += abs(a[i] - b[i]);
+		}
+	
+		result /= a.size();
+
+		return result;
+
+		}
+};
 
 int main() 
 {
-	std::vector<int> v1 = {1, 2, 3};
-	std::vector<int> v2 = {1, 2, 3};
+	std::vector<int> v1 = {1, 1, 1, 1, 1};
+	std::vector<int> v2 = {1, 2, 1, 1, 1};
 
-	double res = mse(v1, v2);
+	Metrics metric;
+
+	double res = metric.mae(v1, v2);
 
 	std::cout << res;
 	
